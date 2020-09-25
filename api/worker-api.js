6de0e5223d01491caeaa95dd7b98d9037c5b248e
Sergei Druzhina <sergei.druzhina@gmail.com>
2020-09-25 15:20:04 +1000

    Updated login page for Google, added routes folder

--- api/worker-api.js
+++ api/worker-api.js
@@ -1,5 +1,5 @@
-var express  = require('express');
-var router = express.Router();
+const express  = require('express');
+const router = express.Router();
 const https = require("https");
 const bodyParser = require("body-parser");
 const Worker = require("../models/Worker");
