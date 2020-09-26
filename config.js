68b9fe2f2554cf99f7e27026e3a31b8ccb59bc60
Sergei Druzhina <sergei.druzhina@gmail.com>
2020-09-26 11:49:31 +1000

    Removed credentials, created new API keys

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const MAILCHIMP_API_KEY = '';
 const MAILCHIMP_LIST_ID = '';
 const GOOGLE = {
-    clientID: '475681532906-hhcht7ere1m659ae0b4hqcv9ati0cvlv.apps.googleusercontent.com',
-    clientSecret: 'AcV5qwTFE9D7G3cYrttPRLFg'
+    clientID: '',
+    clientSecret: ''
 }
 
 exports.api_key = MAILCHIMP_API_KEY;
