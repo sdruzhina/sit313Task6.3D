7c49eb8805e626856748c955c962001bfb5a2cb0
Sergei Druzhina <sergei.druzhina@gmail.com>
2020-09-28 11:50:20 +1000

    Empty config file

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
-const MAILCHIMP_API_KEY = '';
-const MAILCHIMP_LIST_ID = '';
-const GOOGLE = {
-    clientID: '',
-    clientSecret: ''
-}
-
-exports.api_key = MAILCHIMP_API_KEY;
-exports.list_id = MAILCHIMP_LIST_ID;
-exports.google = GOOGLE;
+module.exports = {
+    google: {
+        clientID: '',
+        clientSecret: ''
+    },
+    mongoDB: {
+        uri: ''
+    },
+    mailgun: {
+        api_key: '',
+        domain: ''
+    },
+    keys: {
+        jwtSecret: ''
+    }
+};
